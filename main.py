1f25092803f3d1a1e5812959dd6e4e8c0465def7
Raul Pichardo <raul022107@gmail.com>
2019-09-10 21:55:13 -0400

    Adding db querys

--- main.py
+++ main.py
@@ -22,14 +22,13 @@ def get_input():
 
     return str(user_option) if user_option or user_option == 0 else get_input()
 
-def create_user():
+def create_user(conn):
     name = input("Name: ")
     name = validation.validate_name(name)
     while not name:
         print("Name should be more than 3 charcater and cannot contain any number")
         name = input("Name: ")
         name = validation.validate_name(name)
-
 
     #Email
     email = input("Email: ")
@@ -47,10 +46,13 @@ def create_user():
         number = input("Format: (###.###.####)\nNumber: ") 
         number = validation.validate_number(number)
 
-    print("{}, {}, {}".format(name, email, number))
+    user = (None,name, email, number)
+    sql.create_user(conn, user)
     
 #==Init the program
 if __name__ == "__main__": 
+
+    tables = ['user', 'projects', 'task', 'TASK_USER', 'PROJECT_USER', 'PROJECT_TASK']
 
     conn = sql.create_connection('./db/project_data.db')
     sql.init_db(conn)       
@@ -61,12 +63,14 @@ if __name__ == "__main__":
         3. Create a user
         4. Exit""")
    
-    user_option = get_input()
+    # user_option = get_input()
 
-    option = {"1": True, "2": True, "3": create_user, "4":True}
+    # option = {"1": True, "2": True, "3": create_user, "4":True}
 
-    print(user_option)
-    option[user_option]()
+    # print(user_option)
+    # option[user_option](conn)
+
+    sql.get_table(conn, 'user')
 
     
 
